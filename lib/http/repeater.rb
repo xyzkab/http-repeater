43e88a8e7114c932732af374175d3329334fcfbc
xyzkab <0xyzkab@gmail.com>
2018-10-09 23:47:23 +0700

    add configuration/mixin, :after_configure hook

--- lib/http/repeater.rb
+++ lib/http/repeater.rb
@@ -7,11 +7,15 @@ module HTTP
   class Repeater
     
     extend Chainable
-    extend Configuration
+    extend Configuration::Mixin
 
     def self.request(verb, uri, options = {})
       uri = base_uri ? "#{base_uri}#{uri}" : uri
       branch(options).request verb, uri
+    end
+
+    def self.after_configure
+      @configuration
     end
   end
 end
